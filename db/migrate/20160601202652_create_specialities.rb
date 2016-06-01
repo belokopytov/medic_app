48e7a7d81b4d17c18b14867e984d45f7dc4c8b24
Mikhail Belokopytov <belokopytov@gmail.com>
2016-06-01 23:40:48 +0300

    исправленный скаффолд

--- db/migrate/20160601202652_create_specialities.rb
+++ db/migrate/20160601202652_create_specialities.rb
@@ -1,10 +1,10 @@
 class CreateSpecialities < ActiveRecord::Migration
   def change
     create_table :specialities do |t|
-      t.string, :name
-      t.text, :description
-      t.string, :seo_title
-      t.string, :seo_description
+      t.string :name
+      t.text :description
+      t.string :seo_title
+      t.string :seo_description
       t.string :seo_keywords
 
       t.timestamps null: false
